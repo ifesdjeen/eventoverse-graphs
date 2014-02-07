46d10ae64a9086d331397d5c39776ffecb063c3c
Ken Hawkins <khawkins98@gmail.com>
2014-02-07 12:02:38 +0100

    Adds more notes, features to advanced example

--- assets/javascripts/examples/line-advanced.js
+++ assets/javascripts/examples/line-advanced.js
@@ -8,26 +8,32 @@ JS.require('Eventoverse', function (a) {
             caption: "Random dvalue data, seconds",
             buffer_size: 400
         },
-        interpolate: "cardinal",
+        // Set how the line is drawn
+        // more options at https://github.com/mbostock/d3/wiki/SVG-Shapes
+        interpolate: "linear",
         margin: {
-          top: 500,
-          right: 30,
-          bottom: 30,
-          left: 50
+          top: 0,
+          right: 5,
+          // Setting the bottom to 0 crops off the Y axis
+          bottom: 0,
+          left: 35
         },
-        base_width: 100,
+        base_width: 300,
         base_height: 250
 
     };
 
     line_chart = new Eventoverse.Graphs.Canvas("#line_chart", attrs);
     line_chart.addElement(Eventoverse.Graphs.Lines);
+
+    // You can comment out any of these to disable the feature
     line_chart.addElement(Eventoverse.Graphs.MinLine);
     line_chart.addElement(Eventoverse.Graphs.MaxLine);
     line_chart.addElement(Eventoverse.Graphs.Tooltips, {
-        // tip_formatter: function(d) {
-        //     return "123";
-        // }
+        tip_formatter: function(d) {
+            // Format the tool tips as you like
+            return d.key + ': ' + d.y.toFixed(2) + ' at ' + new Date(d.x *1000);
+        }
     });
 
   vals = [
@@ -49,10 +55,9 @@ JS.require('Eventoverse', function (a) {
       }
     ];
 
-
+    // Or if you want to use random data ...
     // vals = [Eventoverse.RandomData.generate_continuous(20, "key1"),
     //         Eventoverse.RandomData.generate_continuous(20, "key2")];
 
-            // console.log(vals);
     line_chart.render(vals);
 });
